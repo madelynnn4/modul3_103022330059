2b96cbb8745e91370ef40781c17a40f7961c2a2e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-04 08:15:11 +0700

    menambahkan implementasi Kalkulator

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace modul3_103022330059
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int hasil;
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            hasil += Convert.ToInt32(richTextBox1.Text);
+            richTextBox1.Text = "";
+        }
+
+        private void btnHasil_Click(object sender, EventArgs e)
+        {
+            hasil += Convert.ToInt32(richTextBox1.Text);
+            richTextBox1.Text = hasil.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string input = button3.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string input = button4.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string input = button6.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string input = button7.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string input = button8.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string input = button9.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string input = button10.Text;
+            richTextBox1.Text += input;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string input = button11.Text;
+            richTextBox1.Text += input;
         }
     }
 }
